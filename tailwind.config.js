e5f323b6637122d56ef4b6102ebc6e6d6552db1c
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2023-04-22 01:08:09 +0200

    Normalised and installed form plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ["Inter", "sans-serif"]
+            }
+        }
     },
-  },
-  plugins: [],
-}
+    plugins: [require("@tailwindcss/forms")]
+};
