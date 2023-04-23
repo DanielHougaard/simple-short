f8aa75038d856466c5f6f4c1623a8d3136816f51
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2023-04-23 13:36:31 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="w-full bg-slate-900 flex lg:px-80 md:px-40 max-sm:px-5 justify-between items-center py-4">
-      <p className="text-white max-sm:text-sm text-xl">Made by <a href="github.com/danielhougaard" className="text-blue-500 font-medium">Daniel Hougaard</a></p>
-      <p className="text-white max-sm:text-sm text-xl">Source code on <a href="github.com/danielhougaard/url-shortener" className="text-blue-500 font-medium">Github</a></p>
+      <p className="text-white max-sm:text-sm text-xl">Made by <a href="https://github.com/danielhougaard" className="text-blue-500 font-medium">Daniel Hougaard</a></p>
+      <p className="text-white max-sm:text-sm text-xl">Source code on <a href="https://github.com/danielhougaard/simple-short" className="text-blue-500 font-medium">Github</a></p>
     </div>
 
   );
